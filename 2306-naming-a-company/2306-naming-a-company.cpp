747c6000ea66eefe7618e83253be79dcd97baea2
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-02-10 22:58:50 -0500

    Time: 1156 ms (39.63%), Space: 93.4 MB (36.81%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    long long distinctNames(vector<string>& s) {
-        int n = s.size();
-        unordered_map<string,bool> mp;
-        for(auto x:s)
-        {
-            mp[x]=true;
-        }
-        vector<vector<long long >> dict(26,vector<long long> (26,0));
-        for(int i=0;i<n;i++)
-        {
-            string str=s[i].substr(1);
-            int in=s[i][0]-'a';
-            cout<<in<<endl;
-            for(int j=0;j<26;j++){
-                char ch=j+'a';
-                string res=ch+str;
-                if(mp.count(res)==0){
-                    dict[in][j]+=1;
-                }
-            }
-        }
-        long long cnt=0;
-        for(int i=0;i<26;i++)
-        {
-            for(int j=0;j<26;j++){
-                if(dict[i][j]>0 and i!=j)
-                {
-                    cnt+=dict[i][j]*dict[j][i];
-                }
-            }
-        }
-        return cnt;
+    long long distinctNames(vector<string>& ideas) {
+     vector<int> hj(26,0);
+     int n = ideas.size();
+     unordered_map<string,int> mp;
+     long long ans=0;   
+     for(int i=0;i<n;i++) {
+         int in = ideas[i][0]-'a';
+         hj[in]=1;
+         mp[ideas[i]]=1;
+     }   
+     vector<vector<int>> dict(26,vector<int>(26,0));
+     for(int i=0;i<n;i++) {
+         int yu = ideas[i][0]-'a';
+         string res = ideas[i].substr(1);
+         for(int j=0;j<26;j++) {
+             if(hj[j]==1 && j != yu) {
+                 char ch = j+'a';
+                 string jk = ch+res;
+                 if(mp.count(jk)==0)
+                 dict[yu][j]++; 
+             }
+         }
+     } 
+     for(int i=0;i<26;i++)
+     for(int j=0;j<26;j++) {
+         ans+=dict[i][j]*dict[j][i];
+     }    
+     return ans;   
     }
 };
