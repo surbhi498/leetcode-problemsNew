aa4525f8a86dd2006d19a9d5f87138f3ff980b37
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-02-12 10:10:37 -0500

    Time: 15 ms (95.26%), Space: 15.4 MB (45.33%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges,
-                                         vector<vector<int>>& blueEdges) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& redEdge : redEdges) {
-            adj[redEdge[0]].push_back({redEdge[1], 0});
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+     vector<vector<pair<int,int>>> adj(n);
+     for(auto& redEdge : redEdges){
+         adj[redEdge[0]].push_back({redEdge[1],0});
+     }
+        for(auto& blueEdge : blueEdges){
+            adj[blueEdge[0]].push_back({blueEdge[1],1});
         }
-        for (auto& blueEdge : blueEdges) {
-            adj[blueEdge[0]].push_back(make_pair(blueEdge[1], 1));
-        }
-
-        vector<int> answer(n, -1);
-        vector<vector<bool>> visit(n, vector<bool>(2));
         queue<vector<int>> q;
-
-        // Start with node 0, with number of steps as 0 and undefined color -1.
-        q.push({0, 0, -1});
-        visit[0][1] = visit[0][0] = true;
-        answer[0] = 0;
-
-        while (!q.empty()) {
+        vector<int> answer(n,-1);
+        vector<vector<bool>> visit(n, vector<bool>(2));
+        
+        q.push({0,0,-1});
+        answer[0]=0;
+        visit[0][1]=true;
+        visit[0][0]= true;
+        while(!q.empty()) {
             auto element = q.front();
-            int node = element[0], steps = element[1], prevColor = element[2];
+            int node = element[0];
+            int steps = element[1];
+            int prevcolor = element[2];
             q.pop();
-
-            for (auto& [neighbor, color] : adj[node]) {
-                if (!visit[neighbor][color] && color != prevColor) {
-                    visit[neighbor][color] = true;
-                    q.push({neighbor, 1 + steps, color});
-                    if (answer[neighbor] == -1) answer[neighbor] = 1 + steps;
+            for(auto& [neighbor,color] :adj[node]) {
+                if(!visit[neighbor][color] && color != prevcolor) {
+                    visit[neighbor][color]=true;
+                    q.push({neighbor,1+steps, color});
+                    if(answer[neighbor]==-1) answer[neighbor] =1+ steps;
                 }
             }
         }
