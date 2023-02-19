50a6d9a1539ed04adba4c210c356416f708f46ed
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-02-19 03:41:56 -0500

    Time: 7 ms (18.29%), Space: 9.7 MB (7.60%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,57 +1,47 @@
-
 class Solution {
 public:
-
     string addBinary(string a, string b) {
+        int n = a.size();
+        int m = b.size();
+        if(n<m) swap(n,m);
+        cout<<n<<endl;
+        cout<<m<<endl;
+        int jk =m;
+        int carry=0;
         
-        
-        if(b.size() > a.size()) swap(a,b);
-        
-        
-        while(b.size() < a.size()) b = "0" + b;
-
-        int carry = 0;
-
-        string res = "";
-
-        for(int i = b.size()-1; i >= 0 ; --i)
+        while(jk<n) 
         {
-             
-             if(b[i] == '1' && a[i]=='1')
-             {
-
-                if(carry == 0) res = "0" + res;
-                
-                else res = "1" + res;
-                    
-                carry = 1;
-             }
-
-             else if(b[i] =='0' && a[i] =='0')
-             {
-
-                if(carry == 0) res = "0" + res;
-                 
+         if(a.size()>b.size())   
+         b='0'+b;
+         else
+         a='0'+a;    
+         jk++;
+        } 
+        
+        
+        cout<<b<<endl;
+        string res="";
+        for(int j=n-1;j>=0;j--){
+            if(a[j]=='1' && b[j]=='1'){
+            if(carry) res='1'+res;
+            else
+            res='0'+res;
+            carry=1; 
+            cout<<res<<endl;    
+            }
+            else if(a[j]=='0' && b[j]=='0'){
+                if(carry) res='1'+res;
                 else
-                {
-                    res = "1" + res;
-                    carry = 0;
-                }
-             }
-
-             else if((b[i]=='0' && a[i]=='1') || (b[i]=='1' && a[i] == '0'))
-             {
-                 
-                if(carry == 0) res = "1" + res;
-                 
-                else res = "0" + res;
-                 
-             }
-             
-        }
-        
-        if(carry == 1) res = "1" + res;
-        
-        return res;
+                res='0'+res;
+                carry=0;
+            }
+            else if((a[j]=='1'&& b[j]=='0') || (b[j]=='1' && a[j]=='0')){
+                if(carry) {res='0'+res; carry=1;}
+                else
+                {res='1'+res; carry=0;} 
+            }
+           }
+            if(carry) res='1'+res;
+            return res;        
     }
 };
