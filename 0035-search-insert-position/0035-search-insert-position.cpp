09fdee26bbb99ca03bd98d4fc222acc84f22b51b
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-02-21 06:32:30 -0500

    Time: 7 ms (36.76%), Space: 9.6 MB (96.28%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,13 +1,16 @@
 class Solution {
-  public:
-  int searchInsert(vector<int>& nums, int target) {
-    int pivot, left = 0, right = nums.size() - 1;
-    while (left <= right) {
-      pivot = left + (right - left) / 2;
-      if (nums[pivot] == target) return pivot;
-      if (target < nums[pivot]) right = pivot - 1;
-      else left = pivot + 1;
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int i=0;
+        int j = n-1;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) i=mid+1;
+            else if(nums[mid]>target) j=mid-1;
+            
+        }
+        return i;
     }
-    return left;
-  }
-};   
+};
