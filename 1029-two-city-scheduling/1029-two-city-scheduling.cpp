e4cbbe19485389284614bfa19353ceb1306d6f1b
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-02-25 14:46:07 -0500

    Time: 4 ms (80.23%), Space: 8 MB (52.64%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -9,16 +9,16 @@ public:
         int costy=0;
         vector<pair<int,int>> yu;
         for(auto jk : costs){
-            int xc = jk[0]-jk[1];
+            int xc = jk[1]-jk[0];
             yu.push_back({xc,i});
             i++;
         }
         sort(yu.begin(),yu.end());
         for(int i=0;i<(n);i++){
             if(i<(n/2))
-            costy+=costs[yu[i].second][0];
+            costy+=costs[yu[i].second][1];
             else
-             costy+=costs[yu[i].second][1];   
+             costy+=costs[yu[i].second][0];   
         }
         return costy;
         
