339919fca8fa39eed95dc6f3ecacfaa15e2c929b
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-02-25 14:38:19 -0500

    Time: 4 ms (80.23%), Space: 7.9 MB (52.64%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        int countA=0;
+        int countB=0;
+        int i=0;
+        int j=0;
+        int costy=0;
+        vector<pair<int,int>> yu;
+        for(auto jk : costs){
+            int xc = jk[0]-jk[1];
+            yu.push_back({xc,i});
+            i++;
+        }
+        sort(yu.begin(),yu.end());
+        for(int i=0;i<(n);i++){
+            if(i<(n/2))
+            costy+=costs[yu[i].second][0];
+            else
+             costy+=costs[yu[i].second][1];   
+        }
+        return costy;
+        
+    }
+};
