70f519f8aab0ff5cf4c2fea463e72916772c3ba3
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-03-05 13:00:27 -0500

    Time: 54 ms (99.94%), Space: 23.7 MB (64.27%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
                 if(matrix[i][j] != 0){
-                   // if((i+1)<n && (i-1)>=0 && (j-1)>=0 && (j+1)<n)
+                    if((i-1)>=0 && (j-1)>=0)
                     matrix[i][j] += min(matrix[i][j-1], min(matrix[i-1][j-1],matrix[i-1][j]));
                 }
             }
