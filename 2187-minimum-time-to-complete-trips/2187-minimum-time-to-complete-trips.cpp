306617c8dab74e5a4a50fd4d7d20f8d8686f6b82
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-03-11 02:25:56 -0500

    Time: 510 ms (24.66%), Space: 94.6 MB (32.81%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    // Can these buses finish 'totalTrips' of trips in 'givenTime'?
-    bool timeEnough(vector<int>& time, long long givenTime, int totalTrips) {
-        long long actualTrips = 0;
-        for (int t : time) {
-            actualTrips += givenTime / t;
-        }
-        return actualTrips >= totalTrips;
-    }
     long long minimumTime(vector<int>& time, int totalTrips) {
-        // Initialize the left and right boundaries.
-        long long left = 1, right = 1LL * *max_element(time.begin(), time.end()) * totalTrips;
-
-        // Binary search to find the minimum time to finish the task.
-        while (left < right) {
-            long long mid = (left + right) / 2;
-            if (timeEnough(time, mid, totalTrips)) {
-                right = mid;
-            } else {
-                left = mid + 1;
+     int n = time.size();
+     sort(time.begin(),time.end());   
+     long long left = 1;
+     long long right= (long long)totalTrips*time[n-1];
+        while(left<right){
+            long long mid = left+(right-left)/2;
+            long long sum=0;
+            for(int i=0;i<n;i++){
+                sum+=mid/time[i];
+            }
+            if(sum>=totalTrips){
+                right=mid;
+            }
+            else{
+                left = mid+1;
             }
         }
         return left;
