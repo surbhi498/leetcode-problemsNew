e6498f06f871f2fa51dbfadf8589cf644404342a
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-03-11 03:46:34 -0500

    Time: 68 ms (20.28%), Space: 19.1 MB (9.92%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {     
-        // Initalize the left and right boundaries 
-        int left = 1, right = *max_element(piles.begin(), piles.end());
-
-        while (left < right) {
-            // Get the middle index between left and right boundary indexes.
-            // hourSpent stands for the total hour Koko spends.
-            int middle = (left + right) / 2;
-            int hourSpent = 0;
-
-            // Iterate over the piles and calculate hourSpent.
-            // We increase the hourSpent by ceil(pile / middle).
-            for (int pile : piles) {
-                hourSpent += pile / middle + (pile % middle != 0);
+    int minEatingSpeed(vector<int>& piles, int h) {
+         int n = piles.size();
+   //  sort(piles.begin(),piles.end());   
+     int left = 1;
+     int right= *max_element(piles.begin(), piles.end());
+        while(left<right){
+            int mid = left+(right-left)/2;
+            int sum=0;
+            for(int i=0;i<n;i++){
+                sum+=piles[i]/mid + ( piles[i] % mid != 0);
             }
-
-            // Check if middle is a workable speed, and cut the search space by half.
-            if (hourSpent <= h) {
-                right = middle;
-            } else {
-                left = middle + 1;
+            if(sum>h){
+            left = mid +1;
+            }
+            else{
+                right = mid;
             }
         }
-
-        // Once the left and right boundaries coincide, we find the target value,
-        // that is, the minimum workable eating speed.
         return right;
+    
     }
 };
