9882f34405a562adaf804b757774f2f42b2def7a
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-03-17 01:51:31 -0400

    Time: 0 ms (100.00%), Space: 16.4 MB (84.26%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,23 +11,16 @@
  */
 class Solution {
 public:
-    bool isEqual(TreeNode*r1,TreeNode*r2){
-        //if we have both root to nullptr then we will return true
-        //else we will be returning false
-        
-        if(!r1||!r2)
-            return r1==r2;
-        //if not null then we will check for the r1 and r2 values
-        if(r1->val==r2->val){
-        //we will check for the r1 left and r2 right because they will be on opposite sides
-            return isEqual(r1->left,r2->right)&&isEqual(r1->right,r2->left);
-        }
-        //if r1 val not equal to r2 val then return false
-        return false;
+    bool sym1(TreeNode* left, TreeNode* right){
+        if(!left && ! right) return true;
+        if(!left || !right) return false;
+        if(left->val !=right->val) return false;
+        return sym1(left->left, right->right) && sym1(left->right, right->left);
     }
+        
+    
     bool isSymmetric(TreeNode* root) {
-        //The Approach is simple i.e. we will have to check if 
-            //1. the right and left is equal 
-        return isEqual(root->left,root->right);
+       if(!root) return true;
+       return sym1(root->left, root->right);
     }
 };
