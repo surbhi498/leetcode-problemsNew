bb8faad07e0b4189a93db82b35af79c409f4ca46
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-03-24 23:24:08 -0400

    Time: 537 ms (41.99%), Space: 139.8 MB (40.05%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    int bfs(int& n, vector<vector<pair<int, int>>>& adj) {
-        vector<bool> visit(n + 1);
-        queue<int> q;
-        int answer = numeric_limits<int>::max();
-
-        q.push(1);
-        visit[1] = true;
-
-        while (!q.empty()) {
+    queue<int> q;
+   
+    int answer=INT_MAX;
+    int bfs(int n, vector<vector<pair<int,int>>>& adj){
+             vector<bool> visited(n+1,false);
+             q.push(1);
+             visited[1]=true;
+        while(!q.empty()){
             int node = q.front();
             q.pop();
-
-            for (auto& edge : adj[node]) {
-                answer = min(answer, edge.second);
-                if (!visit[edge.first]) {
-                    visit[edge.first] = true;
-                    q.push(edge.first);
+           // if(node==n) break;
+            //answer = min(answer,)
+            for(auto jk : adj[node]){
+                answer = min(answer,jk.second);
+                if(!visited[jk.first]){
+                    visited[jk.first]=true;
+                    q.push(jk.first);
                 }
             }
         }
         return answer;
     }
-
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int, int>>> adj(n + 1);
-        for (auto& road : roads) {
-            adj[road[0]].push_back({road[1], road[2]});
-            adj[road[1]].push_back({road[0], road[2]});
+     //   int n = roads.size();
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto gh : roads){
+            adj[gh[0]].push_back({gh[1],gh[2]});
+            adj[gh[1]].push_back({gh[0],gh[2]});
         }
-        return bfs(n, adj);
+       return bfs(n,adj);
     }
 };
