3c6ab3347dc85bebae6e64f66851a3a5df34e916
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-03-25 11:54:18 -0400

    Time: 144 ms (59.46%), Space: 56.5 MB (16.31%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visit) {
-        visit[node] = true;
-        for (int neighbor : adj[node]) {
-            if (!visit[neighbor]) {
-                dfs(neighbor, adj, visit);
+    void dfs(int n, vector<int>& visited, vector<vector<int>>& adj, int gh){
+        visited[gh]=1;
+        for(auto jk : adj[gh]){
+            if(!visited[jk]){
+                dfs(n,visited,adj,jk);
             }
         }
     }
-
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if (connections.size() < n - 1) {
-            return -1;
-        }
-
         vector<vector<int>> adj(n);
-        for (auto& connection : connections) {
+        for(auto connection : connections){
             adj[connection[0]].push_back(connection[1]);
             adj[connection[1]].push_back(connection[0]);
         }
-
-        int numberOfConnectedComponents = 0;
-        vector<bool> visit(n);
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                numberOfConnectedComponents++;
-                dfs(i, adj, visit);
+        int m = connections.size();
+        if(m<(n-1)) return -1;
+        int numconnected=0;
+        vector<int> visited(n,0);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                numconnected++;
+                dfs(n,visited,adj,i);
             }
         }
-
-        return numberOfConnectedComponents - 1;
+        return numconnected-1;
     }
 };
