736c198eb3cb8ef85b6b592d427be61fd1b2eaa7
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-02 00:16:43 -0400

    Time: 372 ms (19.69%), Space: 110 MB (12.35%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visit) {
-        visit[node] = true;
-        for (int neighbor : adj[node]) {
-            if (!visit[neighbor]) {
-                dfs(neighbor, adj, visit);
-            }
+    bool issimiliar(string a, string b){
+       int n = a.size();
+       int diff=0; 
+       for(int i=0;i<n;i++){
+           if(a[i] != b[i]) diff++;
+       } 
+        if(diff==0 || diff==2) return true;
+        return false;
+    }
+    void dfs(vector<vector<int>>& adj, int start, vector<int>& visited){
+        visited[start]=1;
+        for(auto gh : adj[start]){
+          if(visited[gh]==0){
+              dfs(adj,gh,visited);
+          }  
         }
     }
-
-    bool isSimilar(string& a, string& b) {
-        int diff = 0;
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] != b[i]) {
-                diff++;
-            }
-        }
-        return diff == 0 || diff == 2;
-    }
-
     int numSimilarGroups(vector<string>& strs) {
         int n = strs.size();
-        vector<vector<int>> adj(n);
-        // Form the required graph from the given strings array.
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isSimilar(strs[i], strs[j])) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+        vector<vector<int>> jk(n);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(issimiliar(strs[i],strs[j])){
+                    jk[i].push_back(j);
+                    jk[j].push_back(i);
                 }
             }
+            
         }
-
-        vector<bool> visit(n);
-        int count = 0;
-        // Count the number of connected components.
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                dfs(i, adj, visit);
-                count++;
+        vector<int> visited(n,0);
+        int connectedcompo=0;
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
+                connectedcompo++;
+                dfs(jk,i,visited);
+                
             }
         }
-
-        return count;
+        return connectedcompo;
     }
 };
