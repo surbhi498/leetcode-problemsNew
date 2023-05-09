385b4384c61ea89f1b64b9ced737ead8908c6e87
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-08 21:36:46 -0400

    Time: 16 ms (15.39%), Space: 17.4 MB (65.64%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,35 +12,29 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (root == nullptr) return 0;
-
-        queue<pair<TreeNode*, unsigned long long>> qt;
-        unsigned long long maxwidth = 0;
-        qt.push({root, 0});
-
-        while (!qt.empty()) {
-            int n = qt.size();
-            unsigned long long levelStart = qt.front().second;
-            unsigned long long levelEnd = qt.back().second;
-
-            maxwidth = max(maxwidth, levelEnd - levelStart + 1);
-
-            for (int i = 0; i < n; i++) {
+        if(root==NULL) return 0;
+        queue<pair<TreeNode*, long long>> qt;
+       unsigned long long maxwidth=0;
+        qt.push({root,0});
+        while(!qt.empty()){
+            unsigned  long long n = qt.size();
+            pair<TreeNode*,  unsigned long long> jk = qt.front();
+            for(int i=0;i<n;i++)
+            {
                 pair<TreeNode*, unsigned long long> elm = qt.front();
                 qt.pop();
-
-                TreeNode* node = elm.first;
-                unsigned long long position = elm.second;
-
-                if (node->left != nullptr) {
-                    qt.push({node->left, 2 * position});
+                TreeNode* gh = elm.first;
+                 unsigned long long fg = elm.second;
+                if(gh->left != NULL){
+                    qt.push({gh->left,2*fg});
                 }
-                if (node->right != nullptr) {
-                    qt.push({node->right, 2 * position + 1});
+                if(gh->right != NULL){
+                    qt.push({gh->right, 2*fg+1});
                 }
-            }
+                maxwidth = max(maxwidth,(fg-jk.second+1));
+            }     
+            
         }
-
         return static_cast<int>(maxwidth);
     }
 };
