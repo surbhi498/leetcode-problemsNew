d1cca029d6b2d15e8e76b1756d42f8887434e25a
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-15 11:01:18 -0400

    Time: 404 ms (39.01%), Space: 115.2 MB (69.60%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -13,7 +13,17 @@ public:
     }    
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long> dp(n,-1);
-        return solve1(0,questions,n, dp);
+        vector<long long> dp(n,0);
+        dp[n-1]=questions[n-1][0];
+        for(int i=n-2;i>=0;i--){
+            dp[i]=questions[i][0];
+            int skip = questions[i][1];
+            if((i+skip+1)<n){
+                dp[i]+=dp[i+skip+1];
+            }
+            dp[i]=max(dp[i],dp[i+1]);
+        }
+        //return solve1(0,questions,n, dp);
+        return dp[0];
     }
 };
