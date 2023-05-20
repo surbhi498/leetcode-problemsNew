f21f2d29ce4f4bddc1fa3f6418781cecb234c9e8
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-19 22:03:23 -0400

    Time: 284 ms (45.54%), Space: 126.6 MB (34.84%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,23 +10,55 @@
  */
 class Solution {
 public:
-    int pairSum(ListNode* head) {
-        ListNode* current = head;
-        vector<int> values;
-
-        while (current) {
-            values.push_back(current->val);
-            current = current->next;
+    long long count1(ListNode* head, vector<int>& jk){
+        ListNode* temp = head;
+        
+        long long count=0;
+        while(head != NULL){
+            count++;
+            jk.push_back(head->val);
+            head=head->next;
         }
-
-        int i = 0, j = values.size() - 1;
-        int maximumSum = 0;
-        while (i < j) {
-            maximumSum = max(maximumSum, values[i] + values[j]);
+        return count;
+    }
+    int pairSum(ListNode* head) {
+        if(head==NULL) return 0;
+        vector<int> jk;
+        long long n = count1(head, jk);
+       
+        int max1= INT_MIN;
+        int i=0;
+        int j=jk.size()-1;
+        while(i<j){
+            int vb = jk[i]+jk[j];
+            max1=max(max1,vb);
             i++;
             j--;
         }
-
-        return maximumSum;
+        
+//         for(int i=0;i<(n/2);i++){
+//             int j=i;
+//             int bn = (n-j-1);
+            
+            
+//             ListNode* prev = head;
+//             ListNode* prev1 = head;
+//             while(j>0 && prev != NULL){
+//                 prev=prev->next;
+//                 j--;
+//             }
+//             while(bn>0 && prev1 != NULL){
+//                 prev1=prev1->next;
+//                 bn--;
+//             }
+//             if(prev != NULL && prev1 != NULL){
+            
+                    
+                
+//             max1 = max(max1, prev->val+prev1->val);
+//             }    
+            
+//         }
+        return max1;
     }
 };
