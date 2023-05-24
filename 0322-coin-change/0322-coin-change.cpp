456ea6c10ec3bd27647cc3f36e53b1d603e99f25
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-24 16:48:15 -0400

    Time: 165 ms (19.67%), Space: 86 MB (5.03%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -9,26 +9,36 @@ public:
         vector<vector<long long>> dp(n + 1, vector<long long>(amount + 1, INT_MAX));
         
         // Base case: 0 coins required for amount 0
-        dp[0][0] = 0;
-
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = 0; // 0 coins required for amount 0
+      //  dp[0][0] = 0;
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=amount;i++){
+            if((i%coins[0])==0)
+            {
+                dp[0][i]=i/coins[0];
+            }      
+            
+        } 
+        for (int i = 1; i<n; i++) {
+           // dp[i][0] = 0; // 0 coins required for amount 0
             
             for (int x = 1; x <= amount; x++) {
-                if (coins[i - 1] <= x) {
+                if (coins[i] <= x) {
                     // Choose the minimum between:
                     // 1. Using the current coin: 1 + minimum coins required for amount (x - coin value)
                     // 2. Not using the current coin: coins required from the previous row for the same amount
-                    dp[i][x] = min(1 + dp[i][x - coins[i - 1]], dp[i - 1][x]);
+                    dp[i][x] = min(1 + dp[i][x - coins[i]], dp[i - 1][x]);
                 }
                 else {
                     // If the current coin value is greater than the amount, copy the value from the previous row
+                    //if(i>0)
                     dp[i][x] = dp[i - 1][x];
                 }
             }
         }
 
-        return (dp[n][amount] == INT_MAX) ? -1 : dp[n][amount];
+        return (dp[n-1][amount] == INT_MAX) ? -1 : dp[n-1][amount];
 
     }
 };
