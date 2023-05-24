363902b1a28cda873530c2e64a62535378c36ba1
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-24 16:30:48 -0400

    Time: 184 ms (14.00%), Space: 86 MB (5.03%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,34 @@
+#include <vector>
+#include <climits>
+#include <algorithm>
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-      //  vector<vector<long long>> dp(n,vector<long long>(amount+1,-1));
-        vector<long long> dp(amount+1,-1);
-        // for(long long i=0;i<n;i++){
-        //     dp[i][0]=0;
-        // }
-        dp[0]=0;
-        for(int i=0;i<n;i++){
-            for(int x=1;x<=amount;x++){
-//                 if(coins[i]>x && i>0){
-//                     dp[i]=dp[i-1];
-                    
-//                 }
-               if (coins[i] <= x) {
-                    if (dp[x - coins[i]] != -1) {
-                        if (dp[x] == -1)
-                            dp[x] = dp[x - coins[i]] + 1;
-                        else
-                            dp[x] = min(dp[x], dp[x - coins[i]] + 1);
-                    }
+        vector<vector<long long>> dp(n + 1, vector<long long>(amount + 1, INT_MAX));
+        
+        // Base case: 0 coins required for amount 0
+        dp[0][0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 0; // 0 coins required for amount 0
+            
+            for (int x = 1; x <= amount; x++) {
+                if (coins[i - 1] <= x) {
+                    // Choose the minimum between:
+                    // 1. Using the current coin: 1 + minimum coins required for amount (x - coin value)
+                    // 2. Not using the current coin: coins required from the previous row for the same amount
+                    dp[i][x] = min(1 + dp[i][x - coins[i - 1]], dp[i - 1][x]);
                 }
-                
+                else {
+                    // If the current coin value is greater than the amount, copy the value from the previous row
+                    dp[i][x] = dp[i - 1][x];
+                }
             }
         }
-        return dp[amount];
+
+        return (dp[n][amount] == INT_MAX) ? -1 : dp[n][amount];
+
     }
 };
