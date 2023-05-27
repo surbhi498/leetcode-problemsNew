d8634a2fdff8c45ad1e652116b168a613ecba154
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-27 17:08:00 -0400

    Time: 25 ms (54.48%), Space: 16.2 MB (78.72%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -3,18 +3,36 @@
 class MinStack {
 public:
     stack<int> st;
+    int currentmin=INT_MAX;
     MinStack() {
         
       //  priority_queue<int,vector<int>,greater<int>> df;
     }
     
     void push(int val) {
-        st.push(val);
-      //  df.push(val);
-    }
+        
+        // currentmin = st.top();
+        if(val<=currentmin){
+             st.push(currentmin);
+             currentmin=min(currentmin,val);
+        }
+       
+            
+            st.push(val);
+            //st.push(gh);
+        
+       
     
+    }
+
     void pop() {
+        int yu = st.top();
+    
+    if (yu == currentmin) {
         st.pop();
+        currentmin = st.top();
+    }
+    st.pop();   
         
     }
     
@@ -24,19 +42,21 @@ public:
     }
     
     int getMin() {
-        int min1=INT_MAX;
-        vector<int> gh1;
-        while(!st.empty()){
-            int gh = st.top();
-            gh1.push_back(gh);
-            min1 = min(min1,gh);
-            st.pop();    
-        }
-        reverse(gh1.begin(),gh1.end());
-        for(int i=0;i<gh1.size();i++){
-            st.push(gh1[i]);
-        }
-        return min1;
+        // int min1=INT_MAX;
+        // vector<int> gh1;
+        // while(!st.empty()){
+        //     int gh = st.top();
+        //     gh1.push_back(gh);
+        //     min1 = min(min1,gh);
+        //     st.pop();    
+        // }
+        // reverse(gh1.begin(),gh1.end());
+        // for(int i=0;i<gh1.size();i++){
+        //     st.push(gh1[i]);
+        // }
+        // return min1;
+       // int yu = st.top();
+        return currentmin;
     }
 };
 
