e8bff38864bb44ba5b8c1f3cf527cbe20f65ac56
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-29 11:19:46 -0400

    Time: 5 ms (61.68%), Space: 7.5 MB (81.87%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+//     bool check(vector<int>& piles, int i, int j, int sum1, int sum2,int n){
+//         if(n==0){
+//             if(sum1>sum2){
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         check(piles,i+1,j-1,sum1+piles[i],sum2+piles[j],n-2);
+//         //return false;
+        
+//     }
+    bool stoneGame(vector<int>& piles) {
+        // int n = piles.size();
+        // int start=0;
+        // int end = piles.size()-1;
+        // return check(piles,start,end,0,0,n);
+        return true;
+    }
+};
