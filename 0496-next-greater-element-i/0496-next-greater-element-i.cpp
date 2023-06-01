295d0d1b824854b6e9125b5835faa959845728cf
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-05-31 21:10:37 -0400

    Time: 6 ms (68.56%), Space: 9 MB (16.88%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,40 +3,28 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        stack<int> jk;
-        vector<int> nm;
-        int vb=0;
-        int y=0;
-        int flag=0;
-        for( auto gh : nums1){
-            flag=0;
-            for(vb =0;vb<nums2.size();vb++){
-                if (nums2[vb] != gh){
-                   // jk.push(nums2[vb]);
-                }
-                else{
-                    break;
-                }
-                
+        vector<int> jk(n, -1);
+        stack<pair<int,int>> op;
+        map<int,int> mp;
+
+        for (int i = m - 1; i >= 0; i--) {
+            while (!op.empty() && nums2[i] > op.top().first) {
+                op.pop();
             }
-           // jk.push(nums2[vb]);
-            for(y=vb+1;y<nums2.size();y++){
-                if(nums2[y]<=gh){
-                    continue;
-                }
-                else{
-                    flag=1;
-                    break;
-                }
+
+            if (!op.empty()) {
+                mp[nums2[i]] = op.top().first;
             }
-            if(flag)
-            nm.push_back(nums2[y]);
-            else
-            nm.push_back(-1);
-            // while (!jk.empty()) {
-            //     jk.pop();
-            // }
+
+            op.push(make_pair(nums2[i], i));
         }
-        return nm;
+
+        for (int i = 0; i < n; i++) {
+            if (mp.find(nums1[i]) != mp.end()) {
+                jk[i] = mp[nums1[i]];
+            }
+        }
+
+        return jk;
     }
 };
