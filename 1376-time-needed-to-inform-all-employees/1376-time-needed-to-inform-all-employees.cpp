db15254b3893d01173e183889e859df2a94342cd
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-07 01:03:44 -0400

    Time: 353 ms (39.54%), Space: 129.1 MB (27.09%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int maxTime = INT_MIN;
-    void dfs(int i,vector<int>& visited, vector<vector<int>>& mg,vector<int>& informTime, int sum){
+    void bfs(int i,vector<int>& visited, vector<vector<int>>& mg,vector<int>& informTime, int sum, queue<pair<int,int>>& q){
        visited[i]=1;
-        sum+=informTime[i];
-        maxTime = max(maxTime,sum);
-        for(auto gh : mg[i]){
-           if(!visited[gh]){
-           dfs(gh,visited,mg,informTime,sum);
-         }
-        }
+        while(!q.empty())
+        {
+        int fg = q.front().first;
+        sum = q.front().second;    
+        //sum+=q.front().second; 
+        maxTime = max(maxTime,sum);    
+        q.pop();
+        for(auto jk : mg[fg]){
+            if(!visited[jk]){
+             visited[jk]=1;    
+            q.push({jk,sum+informTime[jk]});
+            }    
+        }    
+        }    
         
     }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         vector<vector<int>> mg(n);
         vector<int> visited(n,0);
         int sum=0;
+        queue<pair<int,int>> q;
+        q.push({headID,informTime[headID]});
         for(int i=0;i<n;i++){
             if(manager[i] != -1)
             mg[manager[i]].push_back(i);
         }
-        dfs(headID,visited,mg,informTime,sum); 
+        bfs(headID,visited,mg,informTime,sum,q); 
         return maxTime;
     }
 };
