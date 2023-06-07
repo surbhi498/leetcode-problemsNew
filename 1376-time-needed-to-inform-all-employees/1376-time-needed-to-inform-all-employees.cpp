c4d1709898b77d264d682b7e1e173294618e494e
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-06 23:59:02 -0400

    Time: 337 ms (52.59%), Space: 124.2 MB (39.04%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int maxTime = INT_MIN;
-    
-    void DFS(vector<int> adjList[], vector<int>& informTime, int curr, int time) {
-        // Maximum time for an employee to get the news.
-        maxTime = max(maxTime, time);
-        
-        for (int adjacent : adjList[curr]) { 
-            // Visit the subordinate employee who gets the news after informTime[curr] unit time.
-            DFS(adjList, informTime, adjacent, time + informTime[curr]);
+    void dfs(int i,vector<int>& visited, vector<vector<int>>& mg,vector<int>& informTime, int sum){
+       // visited[i]=1;
+       
+        maxTime = max(maxTime,sum);
+        for(auto gh : mg[i]){
+           // if(!visited[gh]){
+              
+                dfs(gh,visited,mg,informTime,sum+informTime[i]);
+           // }
         }
+        
     }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<int> adjList[n];
-        
-        // Making an adjacent list, each index stores the Ids of subordinate employees.
-        for (int i = 0; i < n; i++) {
-            if (manager[i] != -1) {
-                adjList[manager[i]].push_back(i);
-            }
+        vector<vector<int>> mg(n);
+        vector<int> visited(n,0);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            if(manager[i] != -1)
+            mg[manager[i]].push_back(i);
         }
-        
-        DFS(adjList, informTime, headID, 0);
+        dfs(headID,visited,mg,informTime,sum); 
         return maxTime;
     }
 };
