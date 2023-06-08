4b0913a7b2ca7f8917000f266783abb479cb17d2
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-08 19:27:13 -0400

    Time: 66 ms (12.84%), Space: 46.6 MB (69.65%) - LeetHub

---
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   
+    long long rec(int i, vector<int>& nums, long long sum, bool gh) {
+        if (i == nums.size()) {
+            if(!gh){
+                long long max1 = LLONG_MIN; 
+                for(auto bn : nums){
+                    if(bn<0 && bn>max1){
+                        max1=bn;
+                    }
+                }
+                 return max1;
+            }
+          
+            return sum;
+        }
+
+      long long take =  rec(i + 1, nums, sum * nums[i],true);
+      long long nottake =  rec(i + 1, nums, sum, gh);
+      return max(take,nottake);
+    }
+
+    long long maxStrength(vector<int>& nums) {
+        bool jk =false;
+       return rec(0, nums, 1,jk);
+       // return max1 == LLONG_MIN ? 0 : max1;
+    }
+};
