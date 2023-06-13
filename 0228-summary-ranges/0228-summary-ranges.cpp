ee7c7629de51da21bef3295fd6ceeb20374859bb
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-13 17:24:15 -0400

    Time: 0 ms (100.00%), Space: 6.9 MB (11.58%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -4,18 +4,13 @@ public:
         int n = nums.size();
         vector<string> mk;
         for(int i=0;i<n;i++){
-            int start=nums[i];
             int j=i+1;
             while((j)<n && (nums[j])==(nums[j-1]+1)){
-              //  start=start+1;
                 j=j+1;
             }
-           
-            cout<<nums[i]<<endl;
             if(nums[i]==nums[j-1])
             {
-                string xc = to_string(nums[i]);
-                mk.push_back(xc);
+                mk.push_back(to_string(nums[i]));
             } 
             else
              mk.push_back(to_string(nums[i])+"->"+to_string(nums[j-1]));
