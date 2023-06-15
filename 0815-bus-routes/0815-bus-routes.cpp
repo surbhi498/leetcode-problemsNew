e0b31254ad1e6bc1ef3f805596bd213899c91db3
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-15 18:06:14 -0400

    Time: 1395 ms (7.30%), Space: 110.1 MB (11.86%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {      int n = routes.size();
+       unordered_map<int,unordered_set<int>> adj;
+       for(int i=0;i<n;i++){
+           for(int j=0;j<routes[i].size();j++){
+               adj[routes[i][j]].insert(i);
+           }
+       }                                                                                  queue<pair<int,int>> q;
+       int bustaken=0;
+       q.push({source,bustaken});
+       unordered_set<int> s;
+       s.insert(source);
+       while(!q.empty()){
+           int currstop=q.front().first;
+           int bustaken=q.front().second;
+           q.pop();
+           if(currstop==target){
+               return bustaken;
+           }
+           for(auto jk : adj[currstop]){
+               for(int r:routes[jk]){
+                   if(s.find(r)==s.end()){
+                      q.push({r,bustaken+1});
+                   }
+               }
+              routes[jk].clear();  
+           }
+          
+       }                                                                                  return -1; 
+        
+    }
+};
