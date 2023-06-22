4930a7ac72a604b13362ac0e255d241417ac70dc
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-21 21:14:48 -0400

    Time: 4 ms (96.78%), Space: 21.1 MB (69.13%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -11,86 +11,38 @@
  */
 class Solution {
 public:
-    int cameras = 0;
-    const int needCamera = -1, covered = 0, haveCamera = 1;
-    int dfs(TreeNode* root) {
-        if(!root) {
-            return covered;
-            /*
-                So that leaf doesn't install camera on it
-                Note: I will never assign camera to a leaf node.
-                It will be better if I assign camera to that leaf's
-                parent node,because parent will cover both its
-                child as well it's own parent as well.  
-            */
+    const int needcamera = -1, covered = 0, havecamera = 1;
+    int camera=0;
+     int dfs(TreeNode* root){
+        
+        if(!root) return covered; 
+        int l1=dfs(root->left);
+        int r1=dfs(root->right); 
+         if(l1 == covered && r1 == covered) {
+          
+            return needcamera;
         }
         
-        int l = dfs(root->left);
-        int r = dfs(root->right);
-        
-        if(l == covered && r == covered) {
-            /*
-                I will tell my parent to cover me because my
-                children are already covered. Installing camera
-                on me will not be benefecial. Let my parent do that
-                because my parent will cover me, my brother and
-                my grandfather as well 
-            */
-            return needCamera;//I need my parent to cover me by a camera on it
-        }
-        
-        if(l == needCamera || r == needCamera) {
-            /*
-                if any of my child wants me to put a camera on me,
-                I will have to put a camera on my self. AND i will tell
-                my parent that I have a camera. Not to worry
-            */
-            cameras++;
-            return haveCamera;
+        if(l1 == needcamera || r1 == needcamera) {
+           
+            camera++;
+            return havecamera;
             
         }
         
-        if(l == haveCamera || r == haveCamera) {
-            /*
-                If any of my child has a camera, I will
-                be covered as well. So, I will tell my parent
-                that I am covered
-            */
+        if(l1 == havecamera || r1 == havecamera) {
+           
             return covered;
         }
-        
-		//don't worry about this case. control will never reach here
-        return -1;
+       return -1;  
     }
     int minCameraCover(TreeNode* root) {
-        /*
-            status code  : what does that mean
-            
-            -1 : I need a camera to be covered
-             0 : covered by either(child or parent)
-             1 : I have a camera so I am covered as well
-                (but if my brother needs a camera my parent will
-                install camera on it)
-            
-        */
-        cameras = 0;
-        int status_of_root = dfs(root);
-        /*
-         Wait Wait, don't directly return cameras here.
-         Just one thing you missed. Take a case as shown below
-                    root
-                    / \
-                   /   \
-                  NULL NULL
-         NULL returns 1 and 1, i.e. they are covered.
-         So, root will tell it's parent to install a camera and it will
-         return -1. But root has no parent. So this case will be missed.
-         So, check the returned value of dfs here and see if it's -1
-         then add one camera and then return cameras
-         
-        */
-        if(status_of_root == needCamera)
-            cameras++;
-        return cameras;
+        camera=0;
+        int status_code=dfs(root);
+        if(status_code==needcamera){
+            camera++;
+          
+        }
+          return camera;
     }
 };
