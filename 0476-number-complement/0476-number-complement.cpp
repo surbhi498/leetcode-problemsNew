e4911badcbfca2fe3391f39d3de543018b740909
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-23 16:44:27 -0400

    Time: 0 ms (100.00%), Space: 5.9 MB (59.22%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
-    string convbin(int num){
-        string gh="";
-        while(num>0){
-            int d = num%2;
-            gh+=(char)(d+'0');
-            num=num/2;
-        }
-        reverse(gh.begin(),gh.end());
-        cout<<gh<<endl;
-        return gh;
-    }
-    int bintoint(string cv){
-        int freshnum=0;
-        int k=0;
-       for(int i=cv.size()-1;i>=0;i--){
-           if(cv[i]=='1'){
-               freshnum+=pow(2,k)*1;
-           }
-           k++;
-          }
-          return freshnum;
-    }
     int findComplement(int num) {
-        string vb = convbin(num);
-        for(int i=0;i<vb.size();i++){
-            if(vb[i]=='1'){
-                vb[i]='0';
-            }
-            else{
-                vb[i]='1';
-            }
+        int op=num;
+        int mask=1;
+        while(op != 0){
+            // flip the current bit
+            num=num^mask;
+            //prepare for next iteration
+            mask=mask<<1;
+            op=op>>1;
         }
-       int ans = bintoint(vb);
-        return ans;
-        
+        return num;
     }
 };
