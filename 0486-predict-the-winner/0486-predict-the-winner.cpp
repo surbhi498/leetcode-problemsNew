0be7afc34e1d3a96dbcb4660fa4d02df8a6d8684
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-28 19:35:52 -0400

    Time: 0 ms (100.00%), Space: 7.7 MB (17.81%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int maxscore(vector<int>& nums, int left, int right){
+    int maxscore(vector<int>& nums, int left, int right, vector<vector<int>>& memo){
+        if(memo[left][right] != -1) return memo[left][right];
         if(left==right){
             return nums[left];
         }
-        int scoreleft = nums[left]-maxscore(nums,left+1,right);
-        int scoreright = nums[right]-maxscore(nums,left,right-1);
-        return max(scoreleft,scoreright);
+        int scoreleft = nums[left]-maxscore(nums,left+1,right,memo);
+        int scoreright = nums[right]-maxscore(nums,left,right-1,memo);
+        memo[left][right]= max(scoreleft,scoreright);
+        return memo[left][right];
     }
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
         int left=0;
         int right=n-1;
-        return maxscore(nums,0,n-1)>=0;
+        vector<vector<int>> memo(n,vector<int>(n,-1));
+        return maxscore(nums,0,n-1,memo)>=0;
     }
 };
