0de27a463e2dc8a4b9338770752660322c58e316
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-29 12:36:31 -0400

    Time: 391 ms (5.29%), Space: 7.4 MB (56.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
+    bool check(int left, int right, int curr_turn, int p1_score, int p2_score, vector<int>& nums){
+        if(left>right) return p1_score>=p2_score;
+        int take_left;
+        int take_right;
+        bool ans;
+        if (curr_turn==1){
+                take_left = check(left+1,right,2,p1_score+nums[left],p2_score, nums);
+                take_right = check(left, right-1,2,p1_score+nums[right], p2_score, nums);
+                if(take_left || take_right){
+                ans=true;
+            }
+            else{
+                ans=false;
+            }
+            
+        }
+        else{
+                take_left = check(left+1,right,1,p1_score,p2_score+nums[left], nums);
+                take_right = check(left, right-1,1,p1_score, p2_score+nums[right], nums);
+                if(take_left && take_right){
+                ans=true;
+            }
+            else{
+                ans=false;
+            }
+            
+        }
+        return ans;
+    }
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int i=0;i<n;i++) dp[i][i]=nums[i];
-        for(int i=1;i<n;i++){
-            for(int j=0;j<n-i;j++){
-                int right=j+i;
-                dp[j][right]=max((nums[j]-dp[j+1][right]),(nums[right]-dp[j][right-1]));
-            }
-        }
-      
-        return dp[0][n-1] >=0;
-       // return dp[0][n-1]>=0;
+        int left=0;
+        int right=n-1;
+        int curr_turn=1;
+        int p1_score=0;
+        int p2_score=0;
+        return check(left,right,curr_turn,p1_score,p2_score, nums);
     }
 };
