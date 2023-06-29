661ec894511c914c05f949e57065a995191ba336
Surbhi Sharma <62746024+surbhi498@users.noreply.github.com>
2023-06-28 23:49:03 -0400

    Time: 0 ms (100.00%), Space: 7.7 MB (17.81%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int maxscore(vector<int>& nums, int left, int right, vector<vector<int>>& memo){
-        if(memo[left][right] != -1) return memo[left][right];
-        if(left==right){
-            return nums[left];
-        }
-        int scoreleft = nums[left]-maxscore(nums,left+1,right,memo);
-        int scoreright = nums[right]-maxscore(nums,left,right-1,memo);
-        memo[left][right]= max(scoreleft,scoreright);
-        return memo[left][right];
-    }
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int left=0;
-        int right=n-1;
-        vector<vector<int>> memo(n,vector<int>(n,-1));
-        return maxscore(nums,0,n-1,memo)>=0;
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++) dp[i][i]=nums[i];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                int right=j+i;
+                dp[j][right]=max((nums[j]-dp[j+1][right]),(nums[right]-dp[j][right-1]));
+            }
+        }
+      
+        return dp[0][n-1] >=0;
+       // return dp[0][n-1]>=0;
     }
 };
